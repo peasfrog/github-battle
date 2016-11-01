587c14f6e3a1a265a2d7a04cfb05fe6ec9c28242
peasfrog <jdsull3002@gmail.com>
2016-11-01 15:11:11 -0700

    code updated to the pure functions chapter

--- app/index.js
+++ app/index.js
@@ -1,15 +1,55 @@
+var USER_DATA = {
+	name:'Jeremy Sullivan',
+	username: 'peasfrog',
+	image: 'https://en.gravatar.com/userimage/111459919/e0757c63a304c70fa37bd4a270c835fb.jpeg'
+
+	
+}
+;
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+	render:function(){
+		return <img src={this.props.imageUrl} style={{height: 100, width:100}}/>
+	}
+});
+
+var ProfileLink = React.createClass({
 	render: function(){
-		return(
-			<div>Hello World!</div>
+		return (
+			<div>
+				<a href= {'https://www.github.com/'+ this.props.username}>
+					{this.props.username}
+				</a>
+			</div>
 			)
 	}
-})	;
+});
+
+var ProfileName = React.createClass({
+	render: function(){
+		return(
+			<div>{this.props.name}</div>	
+			)
+	}
+});
+
+var Avatar = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<ProfilePic imageUrl={this.props.user.image} />
+				<ProfileName name={this.props.user.name} />
+				<ProfileLink username={this.props.user.username} />
+			</div>
+
+		)
+	}
+
+});
 
 ReactDOM.render(
-	<HelloWorld/>, 
-	document.getElementById('app')
-)
+	<Avatar user={USER_DATA}/ >, document.getElementById('app')
+);
